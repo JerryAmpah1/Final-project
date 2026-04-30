081ce3ccd31a7130875e967d0b37e885e9c93a5f
J Ampah <ampahjoseph45@gmail.com>
2026-04-30 14:05:49 -0400

    Map changes

--- Geodata.R
+++ Geodata.R
@@ -3,7 +3,20 @@ library(tidyverse)
 #plot(ne_countries(continent = "Africa"))
 
 africa <- ne_countries(continent = "Africa")
-electricty <- read_csv("International Energy Agency - Total electricity production, Africa.csv")
+electricity_tbl <- read_csv("International Energy Agency - Total electricity production, Africa.csv")
 
 africa <- africa |> 
   select(sov_a3, name_long)
+
+africa_electricity <- left_join(africa, electricity_tbl, by = join_by("sov_a3" =="ISO3")) |>
+  filter(!is.na(Electricity))
+
+plot(africa_electricity)
+
+ggplot(data = africa_electricity) +
+  geom_sf(aes(fill=Electricity))
+
+
+ggplot(data = africa_electricity) +
+  geom_col(mapping = aes(y=sov_a3, x= Electricity))
+  
